2ea2adaa72d2b45e225b33a782cba790bc5b8be6
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-09-05 09:47:56 -0300

    ✨ add typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
+  env: {
+    'jest/globals': true,
+  },
   root: true,
-  extends: '@react-native-community',
-};
+  extends: ['@react-native-community', 'prettier'],
+  plugins: ['jest', 'prettier/prettier'],
+  rules: {
+    semi: ['error', 'never'],
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    'react/require-default-props': ['error'],
+    'react/default-props-match-prop-types': ['error'],
+    'react/sort-prop-types': ['error'],
+    "prettier/prettier": "error",
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off"
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  }
