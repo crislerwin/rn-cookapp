392f483f79e30b27364085c7357cc7f9d0398c18
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-09-05 13:11:35 -0300

    ✅ import toggle and plate component to home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Header } from '../../components/Header';
+import { Plate } from '../../components/Plate';
+import { Toggle } from '../../components/Plate/Toggle';
 import { Container } from './styles';
 
 export const  Home: React.FC<{}> = () => {
   return(
     <Container>
       <Header />
+      <Toggle />
+     <Plate />
     </Container>
   )
 }
