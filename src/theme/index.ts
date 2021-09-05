182e900c311486ecf4bd94e40290669257e88048
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-09-05 10:14:27 -0300

    ✨ add styled-components, fonts and default theme colors

---
+++ src/theme/index.ts
@@ -0,0 +1,22 @@
+import {DefaultTheme} from 'styled-components';
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#0070f3',
+    secondary: '#ff4081',
+    background: '#f55ff3',
+    text: '#000',
+    orange: '#BF612A',
+    white: '#f7f7f7',
+    brown: '#591C27',
+    gray: '#afaca3',
+    red: '#A61B34',
+    fonts: {
+      primary400: 'Ubuntu_400Regular',
+      primary500: 'Ubuntu_500Medium',
+      primary700: 'Ubuntu_700Bold',
+    }
+  }
+}
+
+export default theme;
---
+++ src/theme/theme.d.ts
@@ -0,0 +1,22 @@
+import 'styled-components';
+
+declare module 'styled-components'
+export interface DefaultTheme {
+  title: string;
+  colors: {
+    primary: string
+    secundary: string
+    background: string
+    text: string
+    orange: string
+    white: string
+    brown: string
+    gray: string
+    red: string
+}
+ fonts: {
+   primary400: string
+   primary500: string
+   primary700: string
+ }
+}
