bac9dbd6d85c7574df81e2c15ee5aa48d41a5772
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-09-05 13:16:41 -0300

    ♻️ move toggle folder to components

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Header } from '../../components/Header';
 import { Plate } from '../../components/Plate';
-import { Toggle } from '../../components/Plate/Toggle';
+import { Toggle } from '../../components/Toggle';
 import { Container } from './styles';
 
 export const  Home: React.FC<{}> = () => {
