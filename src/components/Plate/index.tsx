622799f21769b8c78567d00ec1041002d0a16c9f
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-09-05 13:09:59 -0300

    ✅ create a component plate

---
+++ src/components/Plate/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import  {Image, PlateImage} from './styles'
+
+export const Plate : React.FC = () => {
+  return(
+   
+      <Image source={PlateImage} resizeMode="contain"/>
+     
+  )}
---
+++ src/components/Plate/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+import PlateImage from '../../assets/plate.png'
+
+
+const Image = styled.Image`
+flex: 1;
+`;
+
+
+
+export {  Image, PlateImage};
